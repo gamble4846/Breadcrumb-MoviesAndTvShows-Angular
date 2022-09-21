107dc727ce7d24566f475c938e8421a9aefae3e0
Rohan Patel <96464493+Apurva20997@users.noreply.github.com>
2022-09-21 13:11:37 +0530

    Added Routings to all modules

--- Breadcrumb-MoviesAndTvShows/src/app/Modules/TvShows/tv-shows-routing.module.ts
+++ Breadcrumb-MoviesAndTvShows/src/app/Modules/TvShows/tv-shows-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'Home'},
+  {path: 'Home', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- Breadcrumb-MoviesAndTvShows/src/app/app-routing.module.ts
+++ Breadcrumb-MoviesAndTvShows/src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'Home' },
+  {path: '', pathMatch: 'full', redirectTo: 'Home'},
   {path: 'Home', loadChildren: () => import('./Modules/Navigation/navigation.module').then(m => m.NavigationModule)},
+  {path: 'Movies', loadChildren: () => import('./Modules/Movies/movies.module').then(m => m.MoviesModule)},
+  {path: 'TvShows', loadChildren: () => import('./Modules/TvShows/tv-shows.module').then(m => m.TvShowsModule)},
 ];
 
 @NgModule({
