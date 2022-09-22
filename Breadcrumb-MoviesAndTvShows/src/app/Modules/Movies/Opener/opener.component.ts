dc2ebd08214d9944a66753bb04cac9dec16b6883
Rohan Patel <42444158+gamble4846@users.noreply.github.com>
2022-09-23 00:16:00 +0530

    ;.

--- Breadcrumb-MoviesAndTvShows/src/app/Modules/Movies/Opener/opener.component.ts
+++ Breadcrumb-MoviesAndTvShows/src/app/Modules/Movies/Opener/opener.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LocalBaseService } from 'src/app/Services/LocalBase/local-base.service';
 
 @Component({
   selector: 'app-opener',
@@ -10,12 +11,27 @@ export class OpenerComponent implements OnInit {
 
   MovieId:any;
   ServerId:any;
+  MovieTitle:any = {};
+  MovieLinks:any = [];
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private LocalBase: LocalBaseService) { }
 
   ngOnInit(): void {
     this.MovieId = this.route.snapshot.paramMap.get('MovieId');
     this.ServerId = this.route.snapshot.paramMap.get('ServerId');
+    this.getCurrentMovieData();
   }
 
+
+  getCurrentMovieData(){
+    this.LocalBase.GetMoviesTitlesFromLocal().subscribe((response:any) => {
+      this.MovieTitle = response.find((x:any) => x.ServerId == this.ServerId && x.Movies_Id == this.MovieId);
+      console.log(this.MovieTitle);
+    })
+
+    this.LocalBase.GetMoviesLinksFromLocalByServerIdAndMovieId(this.ServerId,this.MovieId).subscribe((response:any) => {
+      this.MovieLinks = response;
+      console.log(this.MovieLinks);
+    })
+  }
 }
